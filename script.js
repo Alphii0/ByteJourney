c1f8b71bbced9bea1f34e8e50aa34d6c554d8431
Alphii0 <152161371+Alphii0@users.noreply.github.com>
2024-02-21 17:56:56 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript para
+interações do usuá
+document.getElemen
+addEventListener("
+    // Lógica para
+se necessário
+    event.preventD
+formulário sej
+    alert("Obrigad
+voluntário!");
+});
